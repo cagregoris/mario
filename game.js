2f10728b0d95a8bdf204913c5798846a09013deb
Carolyn Gregoris <carolyn.a.scobie@gmail.com>
2021-08-15 17:53:21 -0600

    pressing down on pipe takes you to the nect level

--- game.js
+++ game.js
@@ -27,7 +27,7 @@ loadSprite('pipe-top-right', 'hj2GK4n.png')
 loadSprite('pipe-bottom-left', 'c1cYSbt.png')
 loadSprite('pipe-bottom-right', 'nqQ79eI.png')
 
-scene("game", ({ score }) => {
+scene("game", ({ level, score }) => {
   layers(['bg', 'obj', 'ui'], 'obj')
 
   const map = [
@@ -51,8 +51,8 @@ scene("game", ({ score }) => {
     '%': [sprite('surprise'), solid(), 'coin-surprise'],
     '*': [sprite('surprise'), solid(), 'mushroom-surprise'],
     '}': [sprite('unboxed'), solid()],
-    '(': [sprite('pipe-bottom-left'), solid(), scale(0.5)],
-    ')': [sprite('pipe-bottom-right'), solid(), scale(0.5)],
+    '(': [sprite('pipe-bottom-left'), solid(), scale(0.5), 'pipe'],
+    ')': [sprite('pipe-bottom-right'), solid(), scale(0.5), 'pipe'],
     '-': [sprite('pipe-top-left'), solid(), scale(0.5)],
     '+': [sprite('pipe-top-right'), solid(), scale(0.5)],
     '^': [sprite('evil-shroom'), solid(), 'dangerous'], 
@@ -70,7 +70,7 @@ scene("game", ({ score }) => {
     }
   ])
 
-  add([text('level ' + 'test', pos(4, 6))])
+  add([text('level ' + parseInt(level + 1)), pos(40, 6)])
 
   function big() {
     let timer = 0;
@@ -159,6 +159,15 @@ scene("game", ({ score }) => {
     }
   })
 
+  player.collides('pipe', () => {
+    keyPress('down', () => {
+      go('game', {
+        level: (level + 1),
+        score: scoreLabel.value
+      })
+    })
+  })
+
   keyDown('left', () => {
     player.move(-MOVE_SPEED, 0)
   })
@@ -186,4 +195,4 @@ scene('lose', ({ score }) => {
   add([text(score, 32), origin('center'), pos(width()/2, height()/2)])
 })
 
-start("game", { score: 0 })
+start("game", { level: 0, score: 0 })
